0e437e94914dd4c5f0f777b8114e6d770d50e7ce
luishenriquegianeti <169113501+luishenriquegianeti@users.noreply.github.com>
2025-08-26 01:33:35 +0000

    25/08/2025

---
+++ projeto-integrador/src/main/java/entidades/Categoria.java
@@ -0,0 +1,21 @@
+package entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+// em cima do public class faz com que seja global como agora codigo e nome
+@Entity 
+
+public class Categoria {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+
+    // ja dentro do public class e em cima da variavel a aplicação e para aquela variavel como o codigo
+    public Integer codigo;
+
+    public String nome;
+
+    
+}
