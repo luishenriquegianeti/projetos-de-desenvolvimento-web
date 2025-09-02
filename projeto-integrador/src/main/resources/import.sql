d78cc701c6774d5d299d20ae453d6577853a9c8a
luishenriquegianeti <169113501+luishenriquegianeti@users.noreply.github.com>
2025-09-02 01:32:11 +0000

    01/09/2025

---
+++ projeto-integrador/src/main/resources/import.sql
@@ -0,0 +1,28 @@
+insert into Categoria (nome) values ('Bebidas');
+insert into Categoria (nome) Values ('chas');
+insert into Categoria (nome) values ('Carnes');
+insert into Categoria (nome) values ('laticinios');
+insert into Categoria (nome) values ('doces');
+
+
+
+
+
+insert into Cliente (nome,cpf ) values ('eduardo','541.123.312.56');
+insert into Cliente (nome,cpf)  values ('gustavo','234.123.412.22');
+
+
+
+
+
+
+insert into Fabricante (nome) values ('the coca cola Company');
+insert into Fabricante (nome) values ('JBs');
+insert into Fabricante (nome) values ('the pirulitos')
+
+
+
+
+
+insert into Produto (nome,quantidade,preco,validade, Categoria_codigo, Fabricante_codigo) values 'coca cola 2 litros ', '20' ,'12.50','2025-09-01','1','2');
+insert into Produto (nome,quantidade,preco,validade, Categoria_codigo, Fabricante_codigo) values 'pirulitos', '9' ,'2.00','2025-10-09','5','2');
